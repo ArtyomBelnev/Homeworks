0d2130440ed30b939af7ae9c6087818002c7d362
viktorkhv <v.berk98@gmail.com>
2020-04-16 00:41:08 +0300

    Fix remarks
    added: functions in task 2 and 3
    code reduced in task 1 and 2
    deleted unnecessary code in task 3

--- Viktor_Khvesiuk/Homework-18/main.js
+++ Viktor_Khvesiuk/Homework-18/main.js
@@ -1,16 +1,26 @@
 //Задание 1
-var rageexp = /^[a-z]{3,10}_[a-z]{3,10}(\-[0-9]{4})?@[a-z0-9]{1,10}((\-)|(\.))?[a-z0-9]{1,10}(.com)$/.test('name_surname-1234@gmail.com');
+var rageexp = /^[a-z]{3,10}_[a-z]{3,10}(\-\d{4})?@[a-z0-9]{1,10}((\-)|(\.))?[a-z0-9]{1,10}(.com)$/.test('name_surname-1234@gmail.com');
 
 //Задание 2
-var rageexp2 = /^(\+)?((375(\-)?(25|29|33|44|17))|(8(\-)?0(25|29|33|44|17)))(\-)?[1-9]{3}(\-)?[1-9]{2}(\-)?[1-9]{2}$/.test('8-0447777777');
+var rageexp2 = /^(\+)?((375(\-)?)|(8(\-)?0))(25|29|33|44|17)(\-)?[1-9]{1}[0-9]{2}(\-)?[0-9]{2}(\-)?[0-9]{2}$/;
 
+var phone = prompt('Введите номер телефона: ');
+
+telCheck();
+
+function telCheck() {
+    console.log(Boolean(rageexp2.test(phone)))
+}
 //Задание 3
-var str = 'хлеб всему голова';
+var str = prompt('Введите строку для проверки гласных:');
 
-var result = str.match( /[аяоёуюыиеaeiouy]{1,}/gi );
+vowelLetters();
 
-if (result) {
-    console.log('Кол-во гласных: ' + result.length);
-} else {
-    console.log('Здесь нет гласных');
+function vowelLetters() {
+    var result = str.match( /[аяоёуюыиеaeiouy]/gi );
+    if (result) {
+        alert('Кол-во гласных: ' + result.length);
+    } else {
+        alert('Здесь нет гласных');
+    }
 }
